cc35e97ea5c75d498aa7697a2f452f7c7f585fa8
Mahdi <mohammadmahdi.jy@gmail.com>
2026-07-06 22:58:02 +0330

    Remove unused config constant

--- include/Core/Config.hpp
+++ include/Core/Config.hpp
@@ -58,7 +58,7 @@ namespace Config
         constexpr float InitialSupportOffset = 50.f;
         constexpr float InitialY = 550.f;
 
-        constexpr int Count = 10;
+        constexpr int Count = 15;
 
         constexpr float Spacing = 70.f;
 
@@ -72,9 +72,6 @@ namespace Config
         constexpr float MinVerticalCheckDistance = 60.f;
 
         constexpr int MaxSpawnAttempts = 20;
-
-        constexpr float MaxHorizontalStep = 400.f;
-        constexpr float MinHorizontalStep = -400.f;
 
         constexpr float MinVerticalSpacing = 70.f;
         constexpr float MaxVerticalSpacing = 110.f;
